48136e60c7a88f559075907c72e93619661ff388
Sam.Shih <sam.shih@nuetec.com.tw>
2022-04-23 00:07:43 +0800

    update

--- payment/src/main/java/com/samshih/springcloud/controller/PaymentController.java
+++ payment/src/main/java/com/samshih/springcloud/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package com.samshih.springcloud.controller;
 
+import java.util.List;
 import javax.annotation.Resource;
 
 import com.samshih.springcloud.entities.CommonResult;
@@ -7,6 +8,8 @@ import com.samshih.springcloud.entities.Payment;
 import com.samshih.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +25,9 @@ public class PaymentController {
 
 	@Value("${server.port}")
 	private String serverPort;
+
+	@Resource
+	private DiscoveryClient discoveryClient;
 
 	@PostMapping(value = "/payment/create")
 	public CommonResult create(
@@ -49,5 +55,20 @@ public class PaymentController {
 		} else {
 			return new CommonResult(444, "getPaymentById fail, id: " + id, null);
 		}
+	}
+
+	@GetMapping(value = "/payment/discovery")
+	public Object discovery() {
+		List<String> services = discoveryClient.getServices();
+		for (String service : services) {
+			log.info("element: {}", service);
+		}
+
+		List<ServiceInstance> instances = discoveryClient.getInstances("PAYMENT-SERVICE");
+		for (ServiceInstance instance : instances) {
+			log.info("{} {} {} {}", instance.getServiceId(), instance.getHost(), instance.getPort(), instance.getUri());
+		}
+
+		return this.discoveryClient;
 	}
 }
